cc173aab325ea8c9662bd9c35471edfd076263bd
Douglas Lima <100161698+douglasmldcc@users.noreply.github.com>
2022-10-18 22:45:38 -0300

    Lista 1

--- Lista1/bbpseq.c
+++ Lista1/bbpseq.c
@@ -2,23 +2,24 @@
 #include<stdlib.h>
 #include<math.h>
 
-void BBP(int k){
-    float bbpk = (4/(8*k+1))-(2/(8*k+4))-(1/(8*k+5))-(1/(8*k+6));
-    float bbp = bbpk*(1/(powf(16,k)));
-    
-    return bbp;
-}
 
-int main(int argc,char argv[]){
-    float sum=0;
+int main(int argc, char*argv[]){
     int n;
+    double ini,fim,delta;
 
     if(argc < 2) {
-        fprintf(stderr, "Digite: %s <linhas>\n", argv[0]);
+        fprintf(stderr, "Digite: %s <casas decimais>\n", argv[0]);
         return 1;
     }
 
     n = atoi(argv[1]);
+    double pi;
 
 
+    for(double k =0;k<5000;k++){
+        pi +=((1/pow(16,k))*(4/(8*k+1)-  2/(8*k+4)-  1/(8*k+5)-  1/(8*k+6)) );
+    }
+
+    return 0;
 }
+    
